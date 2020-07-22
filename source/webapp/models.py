b569a0e182af3accc49521d73195e04a85d72e39
Alexandr <nuvorish81@gmail.com>
2020-07-22 14:33:43 +0600

    добавленны файлы: html- скопирован с предыдущего проекта, папка static с js,  bootstrap. Добавлена модель To_Do_list и миграция в таблицу

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class To_Do_list(models.Model):
+    description = models.TextField(max_length=3500, null=False, blank=False, verbose_name='Описание')
+    status = models.CharField(max_length=300, null=False, blank=False, default='new', verbose_name='Статус')
+    completion_time = models.DateField(auto_now=False, null=True, blank=True, default=None, verbose_name='Время выполнения')
+
+    def __str__(self):
+        return "{}. {}".format(self.pk, self.description)
--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
-# Create your views here.
+def to_do(request):
+    if request.method =='GET':
+        return render(request, "index_to_do.html")
+    elif request.method == 'POST':
+        print(request.POST)
+        context = {
+            'first_namber': request.POST.get('first_namber', 0),
+            'second_namber': request.POST.get('second_namber', 0),
+            'do': request.POST.get('do', 0),
+            'rezalt': 0
+        }
+
+        return render(request, 'index_to_do.html', context)
